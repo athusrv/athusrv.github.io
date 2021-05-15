2c46db0d1407c394f8837067e517b534d9648437
Athus Vieira <athus.vieira@wexinc.com>
2021-05-15 15:28:27 -0300

    social media buttons with ripple effect

--- athusrv/src/app/components/home/social-media/social-media.module.ts
+++ athusrv/src/app/components/home/social-media/social-media.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SocialMediaComponent } from './social-media.component';
 import { JamIconModule } from "../../common/jam-icon/jam-icon.module";
-
-
+import { MatRippleModule } from "@angular/material/core";
 
 @NgModule({
     declarations: [
@@ -14,7 +13,8 @@ import { JamIconModule } from "../../common/jam-icon/jam-icon.module";
     ],
   imports: [
     CommonModule,
-    JamIconModule
+    JamIconModule,
+    MatRippleModule
   ]
 })
 export class SocialMediaModule { }
